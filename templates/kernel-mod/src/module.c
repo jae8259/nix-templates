b424de3c0fb3355103964f71d8549a27ebe764a1
jae8259 <dragonj5290@gmail.com>
2026-06-24 18:33:59 +0900

    feat(kernel-mod):remove envrc and update module

--- templates/kernel-mod/src/module.c
+++ templates/kernel-mod/src/module.c
@@ -1,19 +1,22 @@
+#define pr_fmt(fmt) "lkp: " fmt
+
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("");
-MODULE_DESCRIPTION("");
+MODULE_AUTHOR("jae8259");
+MODULE_DESCRIPTION("LKP: Description of your module"');
 
 static int __init mod_init(void)
 {
-	pr_info("module: loaded\n");
-	return 0;
+  pr_info("module loaded\n");
+  return 0;
 }
 
 static void __exit mod_exit(void)
 {
-	pr_info("module: unloaded\n");
+  pr_info("module unloaded\n"); /* prints: "Ikp: module loaded" */
 }
 
 module_init(mod_init);
